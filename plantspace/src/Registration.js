8e8b07a2f3402185a0e99c3b50056aa292cab3c3
Susan <susan23codes@gmail.com>
2022-07-31 14:00:45 -0400

    tweaked registration to have a confirm password field as well as handling error if username already exists.  Once registered, routes to login page.

--- plantspace/src/Registration.js
+++ plantspace/src/Registration.js
@@ -26,7 +26,8 @@ export default function Registration(props) {
                 navigate('/login');
             })
             .catch((error) => {
-                setError(error.message)
+                setError(Object.values(error.response.data))
+                console.log(error)
             })
         }
 
@@ -40,26 +41,29 @@ export default function Registration(props) {
    
 
     return (
+        <>
+        {error && <div>{error}</div>}
     <form id="registration-form" onSubmit={handleRegistrationSubmit}>
             <div className="form-controls">
                 <label htmlFor="email-field">Email </label>
-                <input id="email-field" type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)}/>
+                <input id="email-field" type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} required />
             </div>
             <div className="form-controls">
                 <label htmlFor="username-field">Username </label>
-                <input id="username-field" type="text" placeholder="Create a username" onChange={(e) => setUsername(e.target.value)}/> 
+                <input id="username-field" type="text" placeholder="Create a username" onChange={(e) => setUsername(e.target.value)} required/> 
             </div>
             <div className="form-controls">
                 <label htmlFor="password-field">Password </label>
-                <input id="password" type="password" placeholder="Create a password" onChange={(e) => setPassword(e.target.value)}/> 
+                <input id="password" type="password" placeholder="Create a password" onChange={(e) => setPassword(e.target.value)} required/> 
             </div>
             <div className="form-controls">
                 <label htmlFor="confirm-password-field">Confirm Password </label>
-                <input id="confirm-password" type="password" placeholder="Confirm password" onChange={(e) => setConfirmPass(e.target.value)}/> 
+                <input id="confirm-password" type="password" placeholder="Confirm password" onChange={(e) => setConfirmPass(e.target.value)} required /> 
             </div>
             <div className="form-submit">
                 <input type="submit" value="Register"/>
             </div>
         </form>
+        </>
     )
 }
